fb6f5591a18b41bb842bc8bd797c0c26c43a85a5
Pawel Szajna <szajnapawel@gmail.com>
2023-07-14 14:19:42 +0200

    example::E03: Include log excerpts in the comments

--- example/E03_Routing/main.cpp
+++ example/E03_Routing/main.cpp
@@ -71,15 +71,37 @@ int main()
     assert(router.process(EventA{}) == essm::ProcessingStatus::Success);
     assert(router.process(EventB{}) == essm::ProcessingStatus::Success);
 
+    //~ This is reflected in the log:
+    //~
+    //~ ```
+    //~ DBG/ESSMrouter/Router.hpp#49 Received event EventA (0x9cfc03fc)
+    //~ DBG/ESSMservice/EventService.hpp#20 Received event EventA (0x9cfc03fc)
+    //~ INF/Application/main.cpp#32 Received event A in service A
+    //~ DBG/ESSMrouter/Router.hpp#49 Received event EventB (0x5f55246)
+    //~ DBG/ESSMservice/EventService.hpp#20 Received event EventB (0x5f55246)
+    //~ INF/Application/main.cpp#47 Received event B in service B
+    //~ ```
+    //~
     //~ `EventC`, however, has no registered service to handle it, and its processing fails:
 
     assert(router.process(EventC{}) == essm::ProcessingStatus::Failure);
 
+    //~ ```
+    //~ DBG/ESSMrouter/Router.hpp#49 Received event EventC (0x72f262d0)
+    //~ ERR/ESSMrouter/Router.hpp#56 Routing failed: no service registered for EventC (0x72f262d0)
+    //~ ```
+    //~
     //~ If we register `ServiceA` as a handler for `EventD`, the router will send events of this type to this
     //~ service, but the processing fails, as `ServiceA` has no handler for this event type:
 
     router.registerService<EventD>(serviceA);
     assert(router.process(EventD{}) == essm::ProcessingStatus::Failure);
+
+    //~ ```
+    //~ DBG/ESSMrouter/Router.hpp#49 Received event EventD (0xec96f773)
+    //~ DBG/ESSMservice/EventService.hpp#20 Received event EventD (0xec96f773)
+    //~ ERR/ESSMservice/EventService.hpp#28 Service does not offer a handler for event EventD (0xec96f773)
+    //~ ```
 
     return 0;
 }
