91da06fe9e314e806e6f2cbe1449774d0ee3e5d4
Pawel Szajna <szajnapawel@gmail.com>
2023-07-14 13:59:10 +0200

    router: Basic implementation + example

--- example/E03_Routing/main.cpp
+++ example/E03_Routing/main.cpp
@@ -1,9 +1,85 @@
+//~ # Example 3: A simple router
+//~
+//~ This example shows how message handling can be done automatically using a simple event router
+//~ to distribute events to services based on their type.
+
+#include <cassert>
+
 #include <essm/logger/Logger.hpp>
 #include <essm/router/Router.hpp>
+#include <essm/events/Event.hpp>
+#include <essm/service/EventService.hpp>
+
+//~ We define several event types:
+
+essm_event(EventA) {};
+essm_event(EventB) {};
+essm_event(EventC) {};
+essm_event(EventD) {};
+
+//~ We define two services, `ServiceA` which can handle `EventA`, and `ServiceB` which can handle `EventB`:
+
+class ServiceA : public essm::EventService
+{
+public:
+    ServiceA()
+    {
+        registerHandler(&ServiceA::handleA);
+    }
+private:
+    essm::ProcessingStatus handleA(const EventA&)
+    {
+        essm_info("Received event A in service A");
+        return essm::ProcessingStatus::Success;
+    }
+};
+
+class ServiceB : public essm::EventService
+{
+public:
+    ServiceB()
+    {
+        registerHandler(&ServiceB::handleB);
+    }
+private:
+    essm::ProcessingStatus handleB(const EventB&)
+    {
+        essm_info("Received event B in service B");
+        return essm::ProcessingStatus::Success;
+    }
+};
+
+//~ Then we proceed with a few experiments:
 
 int main()
 {
     essm_info("Starting routing demo");
 
+    //~ We set up the router and services:
+
     essm::Router router;
+    ServiceA serviceA{};
+    ServiceB serviceB{};
+
+    //~ We register our instance of `ServiceA` to handle `EventA`, and `ServiceB` to handle `EventB`:
+
+    router.registerService<EventA>(serviceA);
+    router.registerService<EventB>(serviceB);
+
+    //~ When we push the events `A` and `B` to the router, they are distributed to the registered services:
+
+    assert(router.process(EventA{}) == essm::ProcessingStatus::Success);
+    assert(router.process(EventB{}) == essm::ProcessingStatus::Success);
+
+    //~ `EventC`, however, has no registered service to handle it, and its processing fails:
+
+    assert(router.process(EventC{}) == essm::ProcessingStatus::Failure);
+
+    //~ If we register `ServiceA` as a handler for `EventD`, the router will send events of this type to this
+    //~ service, but the processing fails, as `ServiceA` has no handler for this event type:
+
+    router.registerService<EventD>(serviceA);
+    assert(router.process(EventD{}) == essm::ProcessingStatus::Failure);
+
+    return 0;
 }
--- src/router/include/essm/router/Router.hpp
+++ src/router/include/essm/router/Router.hpp
@@ -1,7 +1,71 @@
 #pragma once
 
+#include <essm/types/ProcessingStatus.hpp>
+#include <essm/events/EventTraits.hpp>
+#include <essm/service/EventService.hpp>
+#include <essm/logger/Logger.hpp>
+
+#include <unordered_map>
+
 namespace essm
 {
 class Router
-{};
+{
+
+public:
+
+    /**
+     * @brief Registers a service to handle a message.
+     *
+     * @tparam Event Event type
+     * @param service Service instance
+     *
+     * The user of this method has to take care that the service lives at least as long as the router
+     * (or, in the worst case, that the router receives no requests to process this type of events after
+     * the service is no longer alive).
+     */
+    template<typename Event>
+    void registerService(EventService& service)
+    {
+        __essm_logger_debug("ESSMrouter",
+                            "Registered service for {} ({:#x})",
+                            EventTraits<Event>::name,
+                            EventTraits<Event>::id);
+        services[EventTraits<Event>::id] = &service;
+    }
+
+    /**
+     * @brief Processes the event.
+     *
+     * @param event Event to process
+     * @return Processing status of the event
+     *
+     * The processing will fail (with a Failure status) if there is no handler registered for the passed event.
+     * The processing will result in a segmentation fault if the registered handler had been destructed.
+     */
+    template<typename Event>
+    ProcessingStatus process(const Event& event) const
+    {
+        __essm_logger_debug("ESSMrouter",
+                            "Received event {} ({:#x})",
+                            EventTraits<Event>::name,
+                            EventTraits<Event>::id);
+
+        if (not services.contains(EventTraits<Event>::id))
+        {
+            __essm_logger_error("ESSMrouter",
+                                "Routing failed: no service registered for {} ({:#x})",
+                                EventTraits<Event>::name,
+                                EventTraits<Event>::id);
+            return ProcessingStatus::Failure;
+        }
+
+        return services.at(EventTraits<Event>::id)->handle(event);
+    }
+
+private:
+
+    std::unordered_map<unsigned int, EventService*> services{};
+
+};
 }
