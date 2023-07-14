d206596091ea050ebde3078f1da82a23ed71b43d
Pawel Szajna <szajnapawel@gmail.com>
2023-07-14 16:07:58 +0200

    state_machine: Add basic state machine

--- src/state_machine/include/essm/state_machine/StateMachine.hpp
+++ src/state_machine/include/essm/state_machine/StateMachine.hpp
@@ -1,11 +1,85 @@
 #pragma once
 
+#include <essm/types/ProcessingStatus.hpp>
+#include <essm/types/Traits.hpp>
+#include <essm/logger/Logger.hpp>
 #include <essm/service/EventService.hpp>
+
+#include <optional>
+#include <vector>
 
 namespace essm
 {
 class StateMachine : public EventService
 {
+    struct Transition
+    {
+        using Action = std::optional<std::function<ProcessingStatus(void*)>>;
+
+        types::StateId state{};
+        types::EventId event{};
+        types::StateId target{};
+        Action action{};
+    };
+
+protected:
+
+    template<typename Event>
+    void addTransition(types::StateId state, types::StateId target)
+    {
+        addTransition<Event>(state, target, std::nullopt);
+    }
+
+    template<typename Event, typename ServiceImpl>
+    void addTransition(types::StateId state,
+                       types::StateId target,
+                       ProcessingStatus(ServiceImpl::*handler)(const Event&))
+    {
+        addTransition<Event>(state, target,
+                             Transition::Action{[this, handler](void* event) {
+                                 return (((ServiceImpl*)(this))->*handler)(*(Event*)(event));
+                             }});
+    }
+
+private:
+
+    template<typename Event>
+    void addTransition(types::StateId state, types::StateId target, Transition::Action action)
+    {
+        __essm_logger_debug("ESSMfsm", "Registering transition {} -> {} on {}", state, target, EventTraits<Event>::name);
+        registerHandler<StateMachine, Event>(&StateMachine::forward);
+        transitions.emplace_back(Transition{state, EventTraits<Event>::id, target, std::move(action)});
+    }
+
+    template<typename Event>
+    ProcessingStatus forward(const Event& event)
+    {
+        auto transition = std::find_if(transitions.begin(),
+                                       transitions.end(),
+                                       [state = this->state](const Transition& t)
+                                       {
+                                           return t.state == state and t.event == EventTraits<Event>::id;
+                                       });
+        if (transition == transitions.end())
+        {
+            __essm_logger_warn("ESSMfsm",
+                               "No support for event {} ({:#x}) in current state",
+                               EventTraits<Event>::name,
+                               EventTraits<Event>::id);
+            return ProcessingStatus::Failure;
+        }
+        auto result = ProcessingStatus::Success;
+        if (transition->action)
+        {
+            result = (*transition->action)((void*)(&event));
+        }
+        state = transition->target;
+        __essm_logger_debug("ESSMfsm", "Processing finished, current state is {}", state);
+        return result;
+    }
+
+    types::StateId state{};
+    std::vector<Transition> transitions{};
 
 };
 }
--- src/types/include/essm/types/Traits.hpp
+++ src/types/include/essm/types/Traits.hpp
@@ -6,4 +6,6 @@ namespace essm::types
 {
 using EventId = std::uint32_t;
 using EventName = const char*;
+
+using StateId = std::uint32_t;
 }
